bf435a082e6b4ccedb38bf28ca3a11280673324f
波多野潤一 <hata.junichi@gmail.com>
2018-01-16 00:39:37 +0900

    スタイル整形

--- src/App.vue
+++ src/App.vue
@@ -17,14 +17,14 @@ export default {
 </script>
 
 <style>
+body {
+  background-color: #f0f0f0;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  /* text-align: center; */
   color: #2c3e50;
-  height: 100vh;
-  background-color: #f0f0f0;
   padding-bottom: 100px;
   box-sizing: border-box;
 }
